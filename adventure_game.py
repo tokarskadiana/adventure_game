e0f2d7d910c4fcbebc80ab568830ee3a7da679e7
tokarskadiana <tokarskadiana@com>
2016-11-14 13:15:23 +0100

    add @ to board

--- adventure_game.py
+++ adventure_game.py
@@ -1,15 +1,19 @@
-while True:
-    def game_board():
-        board = [[@]]
-        for row in range(x):
-            board.append([])
-            for kolumn in range(y):
-                if row == 0 or row == x-1 or kolumn == 0 or kolumn == y-1:
-                    board[row].append('x')
-                else:
-                    board[row].append('.')
-        return board
+# while True:
+def game_board(x, y):
+    board = []
+    for row in range(x):
+        board.append([])
+        for kolumn in range(y):
+            if row == 0 or row == x-1 or kolumn == 0 or kolumn == y-1:
+                board[row].append('x')
+            else:
+                board[row].append('.')
+    board[len(board) // 2][len(board[0]) // 2] = '@'
+    return board
 
-    def print_game_board(board):
-        for i in board:
-            print(''.join(i))
+
+def print_game_board(board):
+    for i in board:
+        print(''.join(i))
+
+print_game_board(game_board(22, 80))
